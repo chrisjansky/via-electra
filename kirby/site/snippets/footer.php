97457ebf1b0d458a1561bdce836d97cbdff2ecc3
Christian Jánský <chrisjansky@gmail.com>
2017-09-14 23:16:29 +0200

    Kirby Added proper localized hrefs to snippets/

--- kirby/site/snippets/footer.php
+++ kirby/site/snippets/footer.php
@@ -2,7 +2,7 @@
         <div class="m-footer__wrap">
           <div class="m-footer__flex">
             <div class="m-footer__card">
-              <a class="m-footer__logo c-logo--white" href="<?= url() ?>">
+              <a class="m-footer__logo c-logo--white" href="<?= url($site->language()->code()) ?>">
                 <svg width="39" height="39" viewBox="0 0 39 39" xmlns="http://www.w3.org/2000/svg"><title>Page 1</title><g transform="translate(0 1)" fill="none" fill-rule="evenodd"><path id="Stroke-3" stroke="url(#linearGradient-main)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M37.5 8.375L25.443 8.4l-9.318 10.475-14.25-8.25L12 .125l7.866 4.685"/><path id="Stroke-5" stroke="url(#linearGradient-main)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M25.5 34.625l10.5-6v-15.75l-17.25 9.75v13.5l-15.75-9v-8.25"/><path d="M6 19.625a3 3 0 1 1-6 0 3 3 0 0 1 6 0" id="Fill-7" fill="#1F995C"/><path d="M22.5 4.625a3 3 0 1 1-6 0 3 3 0 0 1 6 0" id="Fill-9" fill="#49AE4F"/><path d="M29.25 33.875a3 3 0 1 1-6 0 3 3 0 0 1 6 0" id="Fill-1" fill="#70C143"/></g></svg><strong class="c-logo__text--main">via electra</strong></a>
               <div class="m-footer__contact">
                 <?= page('contact')->address()->kirbytext() ?>
--- kirby/site/snippets/menu.php
+++ kirby/site/snippets/menu.php
@@ -3,19 +3,19 @@
     <button class="c-menu__close" type="button" data-toggle="menu"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Group 11</title><g transform="translate(3 3)" stroke="#000" stroke-width="3" fill="none" stroke-linecap="square"><path d="M14 0L0 14" id="Line-3"/><path d="M14 14L0 0" id="Line-3-Copy"/></g></svg></button>
     <ul class="c-menu__list">
       <li class="c-menu__item">
-        <a class="c-menu__link" href="<?= url() ?>" data-toggle="menu"><?= page('home')->title() ?></a>
+        <a class="c-menu__link" href="<?= url($site->language()->code()) ?>" data-toggle="menu"><?= page('home')->title() ?></a>
       </li>
       <li class="c-menu__item">
-        <a class="c-menu__link" href="<?= url() ?>#services" data-toggle="menu"><?= page('services')->title() ?></a>
+        <a class="c-menu__link" href="<?= url($site->language()->code()) ?>#services" data-toggle="menu"><?= page('services')->title() ?></a>
       </li>
       <li class="c-menu__item">
-        <a class="c-menu__link" href="<?= url() ?>#work" data-toggle="menu"><?= page('projects')->title() ?></a>
+        <a class="c-menu__link" href="<?= url($site->language()->code()) ?>#work" data-toggle="menu"><?= page('projects')->title() ?></a>
       </li>
       <li class="c-menu__item">
-        <a class="c-menu__link" href="<?= page('about')->url() ?>" data-toggle="menu"><?= page('about')->title() ?></a>
+        <a class="c-menu__link" href="<?= page('about')->url($site->language()->code()) ?>" data-toggle="menu"><?= page('about')->title() ?></a>
       </li>
       <li class="c-menu__item">
-        <a class="c-menu__link" href="<?= page('contact')->url() ?>" data-toggle="menu"><?= page('contact')->title() ?></a>
+        <a class="c-menu__link" href="<?= page('contact')->url($site->language()->code()) ?>" data-toggle="menu"><?= page('contact')->title() ?></a>
       </li>
     </ul>
     <?php foreach ($site->languages() as $language): ?>
