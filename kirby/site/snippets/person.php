15263da4295dcd4697edff0e27398946bdd13e35
Christian Jánský <chrisjansky@gmail.com>
2017-11-25 16:33:16 +0100

    Fixed m-people__item vertical alignment

--- kirby/site/snippets/person.php
+++ kirby/site/snippets/person.php
@@ -1,15 +1,17 @@
 <div class="m-people__item">
-  <span class="m-people__holder">
-    <span class="m-people__ratio">
-      <?php if (!$person->photo()->empty()): ?>
-        <img class="m-people__portrait" data-src="<?= $person->photo()->toFile()->crop(192)->url() ?>" alt="<?= $person->name()->html() ?> Photo">
-      <?php endif ?>
+  <div class="m-people__top">
+    <span class="m-people__holder">
+      <span class="m-people__ratio">
+        <?php if (!$person->photo()->empty()): ?>
+          <img class="m-people__portrait" data-src="<?= $person->photo()->toFile()->crop(192)->url() ?>" alt="<?= $person->name()->html() ?> Photo">
+        <?php endif ?>
+      </span>
     </span>
-  </span>
-  <h3 class="m-people__name"><?= $person->name()->html() ?></h3>
-  <span class="m-people__position" data-grep>
-    <?= $person->job()->html() ?>
-  </span>
+    <h3 class="m-people__name"><?= $person->name()->html() ?></h3>
+    <span class="m-people__position" data-grep>
+      <?= $person->job()->html() ?>
+    </span>
+  </div>
   <ul class="m-people__contact">
     <li>
       <a href="tel:<?= str_replace(" ","",$person->tel()->html()) ?>">
